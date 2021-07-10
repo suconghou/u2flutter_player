34564a0e5dd35c02a4460dca688d1014e0bfc7a1
suconghou <1126045770@qq.com>
2021-07-10 13:25:47 +0800

    save

--- lib/u2flutter_player.dart
+++ lib/u2flutter_player.dart
@@ -1,4 +1,3 @@
-import 'dart:io';
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:brightness_volume/brightness_volume.dart';
@@ -7,48 +6,23 @@ import 'controller_widget.dart';
 import 'video_player_control.dart';
 import 'video_player_pan.dart';
 
-enum VideoPlayerType { network, asset, file }
-
 class VideoPlayerUI extends StatefulWidget {
-  VideoPlayerUI.network({
-    required String url, // 当前需要播放的地址
-    this.width: double.infinity, // 播放器尺寸（大于等于视频播放区域）
-    this.height: double.infinity,
-    this.title = '', // 视频需要显示的标题
-    this.formatHint = VideoFormat.dash,
-  })  : type = VideoPlayerType.network,
-        url = url,
-        super();
-
-  VideoPlayerUI.asset({
-    required String dataSource, // 当前需要播放的地址
-    this.width: double.infinity, // 播放器尺寸（大于等于视频播放区域）
-    this.height: double.infinity,
-    this.title = '', // 视频需要显示的标题
-    this.formatHint = VideoFormat.ss,
-  })  : type = VideoPlayerType.asset,
-        url = dataSource,
-        super();
-
-  VideoPlayerUI.file({
-    required File file, // 当前需要播放的地址
-    this.width: double.infinity, // 播放器尺寸（大于等于视频播放区域）
-    this.height: double.infinity,
-    this.title = '', // 视频需要显示的标题
-    this.formatHint = VideoFormat.ss,
-  })  : type = VideoPlayerType.file,
-        url = file,
-        super();
-
-  final url;
-  final VideoPlayerType type;
   final double width;
   final double height;
   final String title;
-  final VideoFormat? formatHint;
+  final VideoPlayerController controller;
+
+  VideoPlayerUI({
+    required this.controller, // 当前需要播放的地址
+    this.width: double.infinity, // 播放器尺寸（大于等于视频播放区域）
+    this.height: double.infinity,
+    this.title = '', // 视频需要显示的标题
+  });
 
   @override
-  _VideoPlayerUIState createState() => _VideoPlayerUIState();
+  _VideoPlayerUIState createState() {
+    return _VideoPlayerUIState(controller);
+  }
 }
 
 class _VideoPlayerUIState extends State<VideoPlayerUI> {
@@ -58,8 +32,8 @@ class _VideoPlayerUIState extends State<VideoPlayerUI> {
   ///指示video资源是否加载完成，加载完成后会获得总时长和视频长宽比等信息
   bool _videoInit = false;
   bool _videoError = false;
-  bool _first = true;
-  late VideoPlayerController _controller; // video控件管理器
+  bool _destroyed = false;
+  final VideoPlayerController controller;
 
   /// 记录是否全屏
   bool get _isFullScreen =>
@@ -67,39 +41,33 @@ class _VideoPlayerUIState extends State<VideoPlayerUI> {
 
   Size get _window => MediaQueryData.fromWindow(window).size;
 
+  _VideoPlayerUIState(this.controller);
+
   @override
   void initState() {
     super.initState();
-    _urlChange(); // 初始进行一次url加载
+    load(); // 初始进行一次url加载
     BVUtils.keepOn(true); // 设置屏幕常亮
   }
 
   @override
-  void didUpdateWidget(VideoPlayerUI oldWidget) {
-    if (oldWidget.url != widget.url) {
-      _urlChange(); // url变化时重新执行一次url加载
-    }
-    super.didUpdateWidget(oldWidget);
-  }
-
-  @override
-  void dispose() async {
+  void dispose() {
+    _destroyed = true;
+    controller.removeListener(_videoListener);
     super.dispose();
-    _controller.removeListener(_videoListener);
-    _controller.dispose();
-    BVUtils.keepOn(false);
   }
 
   @override
   Widget build(BuildContext context) {
+    final full = _isFullScreen;
     return SafeArea(
-      top: !_isFullScreen,
-      bottom: !_isFullScreen,
-      left: !_isFullScreen,
-      right: !_isFullScreen,
+      top: !full,
+      bottom: !full,
+      left: !full,
+      right: !full,
       child: Container(
-        width: _isFullScreen ? _window.width : widget.width,
-        height: _isFullScreen ? _window.height : widget.height,
+        width: full ? _window.width : widget.width,
+        height: full ? _window.height : widget.height,
         child: _isHadUrl(),
       ),
     );
@@ -107,38 +75,29 @@ class _VideoPlayerUIState extends State<VideoPlayerUI> {
 
 // 判断是否有url
   Widget _isHadUrl() {
-    if (widget.url != null) {
-      return ControllerWidget(
-        controlKey: _key,
-        controller: _controller,
-        videoInit: _videoInit,
-        title: widget.title,
-        child: VideoPlayerPan(
-          child: Container(
-            alignment: Alignment.center,
-            width: double.infinity,
-            height: double.infinity,
-            color: Colors.black,
-            child: _isVideoInit(),
-          ),
+    return ControllerWidget(
+      controlKey: _key,
+      controller: controller,
+      videoInit: _videoInit,
+      title: widget.title,
+      child: VideoPlayerPan(
+        child: Container(
+          alignment: Alignment.center,
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.black,
+          child: _isVideoInit(),
         ),
-      );
-    } else {
-      return Center(
-        child: Text(
-          '暂无视频信息',
-          style: TextStyle(color: Colors.white),
-        ),
-      );
-    }
+      ),
+    );
   }
 
 // 加载url成功时，根据视频比例渲染播放器
   Widget _isVideoInit() {
     if (_videoInit) {
       return AspectRatio(
-        aspectRatio: _controller.value.aspectRatio,
-        child: VideoPlayer(_controller),
+        aspectRatio: controller.value.aspectRatio,
+        child: VideoPlayer(controller),
       );
     } else if (_videoError) {
       return Text(
@@ -156,57 +115,62 @@ class _VideoPlayerUIState extends State<VideoPlayerUI> {
     }
   }
 
-  void _urlChange() async {
-    if (widget.url == null || widget.url == '') return;
-    if (!_first) {
-      /// 如果控制器存在，清理掉重新创建
-      _controller.removeListener(_videoListener);
-      _controller.dispose();
-    }
-    _first = false;
-
-    setState(() {
-      /// 重置组件参数
-      _videoInit = false;
-      _videoError = false;
-    });
-    if (widget.type == VideoPlayerType.file) {
-      _controller = VideoPlayerController.file(
-        widget.url,
-      );
-    } else if (widget.type == VideoPlayerType.asset) {
-      _controller = VideoPlayerController.asset(widget.url);
-    } else {
-      _controller = VideoPlayerController.network(widget.url,
-          formatHint: widget.formatHint);
-    }
+  void load() async {
+    controller.removeListener(_videoListener);
 
     /// 加载资源完成时，监听播放进度，并且标记_videoInit=true加载完成
-    _controller.addListener(_videoListener);
-    await _controller.initialize();
+    controller.addListener(_videoListener);
+    if (_videoInit || controller.value.isInitialized) {
+      if (_destroyed) {
+        return;
+      }
+      setState(() {
+        _videoInit = true;
+        _videoError = controller.value.hasError;
+        controller.play();
+      });
+      return;
+    }
+    // 此处不需要等待完成
+    await controller.initialize();
+    if (_destroyed) {
+      return;
+    }
     setState(() {
       _videoInit = true;
       _videoError = false;
-      _controller.play();
+      controller.play();
     });
   }
 
   void _videoListener() async {
-    if (_controller.value.hasError) {
+    if (_destroyed) {
+      controller.removeListener(_videoListener);
+      return;
+    }
+    if (controller.value.isInitialized || controller.value.isPlaying) {
+      setState(() {
+        _videoInit = true;
+      });
+    }
+    if (controller.value.hasError) {
       setState(() {
         _videoError = true;
       });
     } else {
-      Duration? res = await _controller.position;
-      if (res != null && res >= _controller.value.duration) {
-        await _controller.seekTo(Duration(seconds: 0));
-        await _controller.pause();
+      Duration? res = await controller.position;
+      if (res == null) {
+        return;
       }
-      if (_controller.value.isPlaying && _key.currentState != null) {
+      if (res >= controller.value.duration) {
+        await controller.seekTo(Duration(seconds: 0));
+        await controller.pause();
+      }
+      if (controller.value.isPlaying && _key.currentState != null) {
         /// 减少build次数
         _key.currentState?.setPosition(
           position: res,
-          totalDuration: _controller.value.duration,
+          totalDuration: controller.value.duration,
         );
       }
     }
